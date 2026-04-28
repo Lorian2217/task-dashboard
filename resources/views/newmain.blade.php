752617f638559c6ed63250899eea9fd8c95aa4c1
Loran <danilmockhnatkin@gmail.com>
2026-04-28 09:30:05 +0400

    Вывод данных на детальную карточку

--- resources/views/newmain.blade.php
+++ resources/views/newmain.blade.php
@@ -25,7 +25,7 @@
 
         <div class="task-list flex flex-col gap-4">
             @foreach ($tasks as $task)
-                <div class="item relative select-none flex flex-col" data-modal-target="default-modal" data-modal-toggle="default-modal">
+                <div class="item relative select-none flex flex-col" data-modal-target="default-modal-{{ $task['id'] }}" data-modal-toggle="default-modal-{{ $task['id'] }}">
                     <span class="name w-auto text-2xl font-semibold">{{ $task['name'] }}</span>
                     <span class="text-lg">{{ $task['project'] }}</span>
                     <span class="data_inactive text-sm absolute block z-1 bg-red-200 rounded-2xl">{{ $task['data_inactive'] }}</span>
@@ -35,37 +35,67 @@
         </div>
 
         <!-- Main modal -->
-        <div id="default-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full backdrop-blur-md">
-            <div class="relative p-4 w-full max-w-2xl max-h-full">
-                <!-- Modal content -->
-                <div class="relative bg-neutral-primary-soft border border-default rounded-base shadow-sm p-4 md:p-6">
-                    <!-- Modal header -->
-                    <div class="flex items-center justify-between border-b border-default pb-4 md:pb-5">
-                        <h3 class="text-lg font-medium text-heading">
-                            Terms of Service
-                        </h3>
-                        <button type="button" class="text-body bg-transparent hover:bg-neutral-tertiary hover:text-heading rounded-base text-sm w-9 h-9 ms-auto inline-flex justify-center items-center" data-modal-hide="default-modal">
-                            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6"/></svg>
-                            <span class="sr-only">Close modal</span>
-                        </button>
-                    </div>
-                    <!-- Modal body -->
-                    <div class="space-y-4 md:space-y-6 py-4 md:py-6">
-                        <p class="leading-relaxed text-body">
-                            With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-                        </p>
-                        <p class="leading-relaxed text-body">
-                            The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-                        </p>
-                    </div>
-                    <!-- Modal footer -->
-                    <div class="flex items-center border-t border-default space-x-4 pt-4 md:pt-5">
-                        <button data-modal-hide="default-modal" type="button" class="text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">I accept</button>
-                        <button data-modal-hide="default-modal" type="button" class="text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">Decline</button>
+        @foreach ($tasks as $task)
+            <div id="default-modal-{{ $task['id'] }}" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full backdrop-blur-md">
+                <div class="relative p-4 w-full max-w-2xl max-h-full">
+                    <!-- Modal content -->
+                    <div class="task-modal relative bg-neutral-primary-soft border border-default rounded-base shadow-sm p-4 md:p-6">
+                        <!-- Modal header -->
+                        <div class="flex items-center justify-between border-b border-default pb-4 md:pb-5">
+                            <div class="text-xl md:text-2xl font-medium text-heading"> {{ $task['name'] }} </div>
+                            <button type="button" class="text-body bg-transparent hover:bg-neutral-tertiary hover:text-heading rounded-base text-sm w-9 h-9 ms-auto inline-flex justify-center items-center" data-modal-hide="default-modal-{{ $task['id'] }}">
+                                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6"/></svg>
+                                <span class="sr-only">Close modal</span>
+                            </button>
+                        </div>
+                        <!-- Modal body -->
+                        <div class="info space-y-4 md:space-y-6 py-4 md:py-6">
+                            <div class="flex flex-col gap-2 md:gap-3">
+                                <div class="text-lg">Описание задачи:</div>
+                                <div class="text-body leading-relaxed">{{ $task['description'] }}</div>
+                            </div>
+                            <div class="flex items-center gap-2 md:gap-3">
+                                <div class="text-lg">Ответственный:</div>
+                                <div>{{ $task['executor'] }}</div>
+                            </div>
+                            <div class="flex items-center gap-2 md:gap-3">
+                                <div class="text-lg">Менеджер:</div>
+                                <div>{{ $task['manager'] }}</div>
+                            </div>
+                            <div class="flex items-center gap-2 md:gap-3">
+                                <div class="text-lg">Дата создания:</div>
+                                <div>{{ $task['data_active'] }}</div>
+                            </div>
+                            <div class="flex items-center gap-2 md:gap-3">
+                                <div class="text-lg">Крайний срок:</div>
+                                <div>{{ $task['data_inactive'] }}</div>
+                            </div>
+                            <div class="flex items-center gap-2 md:gap-3">
+                                <div class="text-lg">Приоритет:</div>
+                                <div>{{ $task['priority'] }}</div>
+                            </div>
+                            <div class="flex items-center gap-2 md:gap-3">
+                                <div class="text-lg">Проект:</div>
+                                <div>{{ $task['project'] }}</div>
+                            </div>
+                            <div class="flex flex-col gap-2 md:gap-3">
+                                <div class="text-lg">Прикреплённые файлы:</div>
+                                <div>{{ $task['files'] }}</div>
+                            </div>
+                            <div class="flex flex-col gap-2 md:gap-3">
+                                <div class="text-lg">Комментарии:</div>
+                                <textarea name="comments" class="select-none rounded-md bg-gray-100 border-none" disabled>{{ $task['comments'] }}</textarea>
+                            </div>
+                        </div>
+                        <!-- Modal footer -->
+                        <div class="flex items-center border-t border-default space-x-4 pt-4 md:pt-5">
+                            <button data-modal-hide="default-modal-{{ $task['id'] }}" type="button" class="text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">I accept</button>
+                            <button data-modal-hide="default-modal-{{ $task['id'] }}" type="button" class="text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">Decline</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        @endforeach
 
     </div>
 
